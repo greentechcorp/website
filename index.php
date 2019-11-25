01886422897920bb98187ad2ae109ec382796b6f
David Jay <david@c033043.wlan.net.ed.ac.uk>
2019-11-25 18:13:27 +0000

    Added the google maps section with a maps widget.

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ if (isset($_SESSION['loggedin'])) {
       <li><a href="#home">Homepage</a></li>
       <li><a href="#products">Products</a></li>
       <li><a href="#team">Team</a></li>
-      <li><a href="contact.html">Contact</a></li>
-      <li><a href="news.html">News</a></li>
+      <li><a href="#maps">Map</a></li>
+      <li><a href="#contact">Contact</a></li>
       <li style="float:right" ><a href="login.php">Login</a></li>
       <li style="float:right" ><a href="blog.php">Blog</a></li>
     </ul>
@@ -63,12 +63,54 @@ if (isset($_SESSION['loggedin'])) {
     </div>
   </div>
 
-  <section id="contact" class="section section-dark">
+
+  <section id="maps" class="section section-dark">
+     <h2>Maps</h2>
+
+     <head>
+    <style>
+       /* Set the size of the div element that contains the map */
+      #map {
+        height: 400px;  /* The height is 400 pixels */
+        width: 100%;  /* The width is the width of the web page */
+       }
+    </style>
+    </head>
+    <body>
+      <!--The div element for the map -->
+      <div id="map"></div>
+      <script>
+        // Initialize and add the map
+        function initMap() {
+          // The location of Uluru
+          var AT = {lat: 55.9444, lng: -3.1870};
+          // The map, centered at Uluru
+          var map = new google.maps.Map(
+              document.getElementById('map'), {zoom: 16, center: AT});
+          // The marker, positioned at Uluru
+          var marker = new google.maps.Marker({position: AT, map: map});
+        }
+      </script>
+      <!--Load the API from the specified URL
+      * The async attribute allows the browser to render the page while the API loads
+      * The key parameter will contain your own API key (which is not needed for this tutorial)
+      * The callback parameter executes the initMap() function
+      -->
+      <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArLQlqxDaDLUqsu7KnMhl0Ud1FNIA9-zo&callback=initMap">
+      </script>
+    </body>
+
+
+  </section>
+
+  <section id="contact" class="section section-light">
     <h2>Contact</h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium, blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in.
     </p>
   </section>
+
 
   <div class="section1">
     <div class="ptext">
